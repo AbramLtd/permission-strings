259869b30c2c0a7c2f7b58961e203b3401d073c0
David Abram <david@abram.com>
2018-07-31 21:12:35 +0200

    update

--- __tests__/isPermited.test.js
+++ __tests__/isPermited.test.js
@@ -7,4 +7,6 @@ test('it is permited', () => {
   expect(isPermited(['user:*:*'], ['user:get:1'])).toEqual(true);
   expect(isPermited(['user:get:1'], ['user:post:1'])).toEqual(false);
   expect(isPermited(['user:get,post:1'], ['user:post:1'])).toEqual(false);
+  expect(isPermited(['user:get,post:1', 'login:get:*'], ['user:post:1', 'login:get:1'])).toEqual(false);
+  expect(isPermited(['user:get,post:*', 'login:get:*'], ['user:get,post:1', 'login:get:1'])).toEqual(true);
 });
