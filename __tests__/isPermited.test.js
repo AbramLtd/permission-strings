c4881315a6adb79e3b3c4efe78ceda0882a7228b
David Abram <david@abram.com>
2018-12-07 18:43:40 +0100

    added circleci + coverage

--- __tests__/isPermited.test.js
+++ __tests__/isPermited.test.js
@@ -2,6 +2,8 @@ const isPermited = require('../');
 
 test('it is permited', () => {
   expect(isPermited(['user:get:1'], ['user:get:1'])).toEqual(true);
+  expect(isPermited(['user:get:*'], ['user:get:*'])).toEqual(true);
+  expect(isPermited(['user:get:1'], ['user:get:*'])).toEqual(false);
   expect(isPermited(['user:get:1'], ['apple:get:1'])).toEqual(false);
   expect(isPermited(['user:get:*'], ['user:get:1'])).toEqual(true);
   expect(isPermited(['user:*:*'], ['user:get:1'])).toEqual(true);
